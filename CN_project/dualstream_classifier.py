70d7b40e1bd9d2c13233be34f645bb378251f3be
ejkemper <esther-kemper@live.nl>
2020-10-01 21:43:32 +0200

    dual stream which now takes and image and a cochleagram of the same digit works. The classification accuracy is 95%. For now it can only take an image and a cochleagram. Not able to take 4 inputs yet. Yet to be implemented.

--- CN_project/dualstream_classifier.py
+++ CN_project/dualstream_classifier.py
@@ -8,32 +8,43 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from tensorflow.python.keras import Model
 
 
-class Autoencoder(Model):
-    def __init__(self, latent_dim):
-        super(Autoencoder, self).__init__()
+class DualStreamClassifier(Model):
+    def __init__(self):
+        super(DualStreamClassifier, self).__init__()
 
         self.encoder1_im = tf.keras.Sequential([
-            keras.layers.Flatten(input_shape=(28, 28)),     #input
+            keras.layers.Flatten(input_shape=(28, 28)),     #input im
             keras.layers.Dense(32, activation='sigmoid')
         ])
 
         self.encoder1_ch = tf.keras.Sequential([
-            keras.layers.Flatten(input_shape=(15, 53)),
+            keras.layers.Flatten(input_shape=(15, 53)),     #input coch
             keras.layers.Dense(32, activation='sigmoid')
         ])
 
-        self.decoder = tf.keras.Sequential([
-
+        self.combine = tf.keras.Sequential([
+            keras.layers.Concatenate(axis=-1),
+            keras.layers.Dense(32, activation='sigmoid')
         ])
-    def call(self, im, ch):
+
+        self.classify = tf.keras.Sequential([
+            keras.layers.Dense(10, activation='sigmoid')
+        ])
+
+
+    def call(self, input):
+        im, ch = input
         encoded_im = self.encoder1_im(im)
         encoded_ch = self.encoder1_ch(ch)
-        decoded = self.decoder()
-        return decoded
+        combined = self.combine([encoded_im, encoded_ch])
+        classified = self.classify(combined)
+        return classified
 
-def load_data(dir):
+
+def load_files(dir): #coch=15,53,
     filenames = listdir(dir)
     files = np.zeros((len(filenames), 15, 53))
     labels = []
@@ -46,16 +57,45 @@ def load_data(dir):
     return files, labels
 
 
+def match_data(data_mnist, labels_mnist, data_coch, labels_coch):
+    data_ch = []
+    data_m = []
+    labels = []
+    for x in range(10):
+        nr_mnist = len(labels_mnist[labels_mnist == x])
+        nr_coch = len(labels_coch[labels_coch == x])
+        reps = np.ceil(nr_mnist/nr_coch)
+
+        rep_data_ch = np.repeat(data_coch[labels_coch == x], reps, axis=0)
+        rep_data_ch = rep_data_ch[:nr_mnist]
+        data_ch.append(rep_data_ch)
+
+        data_m.append(data_mnist[labels_mnist == x])
+        labels.append(labels_mnist[labels_mnist == x])
+
+    data_ch = np.concatenate(data_ch, axis=0)
+    data_m = np.concatenate(data_m, axis=0)
+    data = [data_m, data_ch]
+    labels = np.concatenate(labels, axis=0)
+    return data, labels
+
 
 if __name__ == '__main__':
     dir = "/home/esther/Documents/Uni/SB/Block7/Computational neuro/project/output"
-    data, labels = load_data(dir)
-    train_data, test_data, train_label, test_label = train_test_split(data, labels, test_size=0.2, random_state=42)
+    data_coch, labels_coch = load_files(dir)
+    train_data_ch, test_data_ch, train_label_ch, test_label_ch = train_test_split(data_coch, labels_coch, test_size=0.2, random_state=42)
 
-    model = Autoencoder(latent_dim=64)
+    mnist = keras.datasets.mnist
+    (train_data_mnist, train_label_mnist), (test_data_mnist, test_label_mnist) = mnist.load_data()
+
+    model = DualStreamClassifier()
 
     model.compile(optimizer='adam',
-                  loss=tf.keras.losses.MeanSquaredError())
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                  metrics=['accuracy'])
+
+    train_data, train_label = match_data(train_data_mnist, train_label_mnist, train_data_ch, train_label_ch)
+    test_data, test_label = match_data(test_data_mnist, test_label_mnist, test_data_ch, test_label_ch)
 
     model.fit(train_data, train_label, epochs=50, shuffle=True, validation_data=(test_data, test_label))
     test_loss = model.evaluate(test_data, test_label, verbose=2)
